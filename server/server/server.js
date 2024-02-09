f29df69e697c38911030d02758404ef3f2623b6b
mab9913 <mabpalavecino@outlook.es>
2024-02-09 12:04:54 +0100

    actualizacion cors

--- server/server/server.js
+++ server/server/server.js
@@ -2,52 +2,50 @@ const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
 const helmet = require('helmet');
-const path = require('path'); // Ya lo tenías para manejar rutas de archivos
+const path = require('path');
 const authController = require('../controllers/authController');
 const dataController = require('../controllers/dataController');
 
 const app = express();
+
 app.use(
     helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        connectSrc: ["'self'", "http://165.227.226.111:9000", "https://pinaspot.com"], // Asegúrate de agregar aquí todas las URLs externas que tu aplicación necesita contactar
-        imgSrc: ["'self'", "data:", "*.tile.openstreetmap.org"],
-        // Agrega aquí otras directivas según sea necesario
-      },
+        directives: {
+            defaultSrc: ["'self'"],
+            connectSrc: ["'self'", "http://165.227.226.111:9000", "https://pinaspot.com", "ws://165.227.226.111:9000", "wss://pinaspot.com"], // Asegúrate de agregar aquí todas las URLs externas que tu aplicación necesita contactar
+            imgSrc: ["'self'", "data:", "*.tile.openstreetmap.org"],
+            // Agrega aquí otras directivas según sea necesario
+        },
     })
-  );
-  
+);
 
-// app.use(helmet());
+// Configuración de CORS
+// Aquí, establecemos explícitamente los orígenes permitidos en lugar de usar '*'
+const allowedOrigins = ['https://pinaspot.com', 'http://165.227.226.111:3000', 'http://localhost:3000'];
 
-const allowedOrigins = ['http://165.227.226.111:3000', 'http://localhost:3000'];
 app.use(cors({
-    origin:'*',
-    credentials:true,
-}))
-// app.use(cors({
-//     credentials: true,
-//     origin: (origin, callback) => {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Origen no permitido por CORS'));
-//         }
-//     }
-// }));
-
+    origin: function (origin, callback) {
+        // Permitir solicitudes sin 'origin' (como aplicaciones móviles o curl)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'La política de CORS para este sitio no permite el acceso desde el origen especificado.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true,
+}));
 
 app.use(express.json());
 
 // Configuración de express-session
 app.use(session({
-    secret: 'Bonica10_01', // Reemplaza 'tuSecreto' con una cadena secreta fuerte
+    secret: 'Bonica10_01',
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true
+        secure: process.env.NODE_ENV === 'production', // Asegúrate de que las cookies solo se transmitan sobre HTTPS.
+        httpOnly: true // Minimiza el riesgo de ataques XSS.
     }
 }));
 
@@ -56,7 +54,7 @@ const isAuthenticated = (req, res, next) => {
     if (req.session.userId) {
         next();
     } else {
-        console.log('Usuario no autorizado'); // Agrega un registro de depuración
+        console.log('Usuario no autorizado');
         res.status(401).json({ error: 'No autorizado' });
     }
 };
@@ -66,76 +64,13 @@ app.post('/login', authController.login);
 app.get('/new-luminaries', isAuthenticated, dataController.getNewLuminaries);
 app.put('/update-luminaries/:id', isAuthenticated, dataController.updateLuminary);
 
-// Ajuste para la ruta de la carpeta build
-// Cambio clave: Asume que `server.js` se ejecuta desde el subdirectorio 'server/server' y que la carpeta 'build' está dos niveles arriba
+// Servir archivos estáticos de React
 app.use(express.static(path.join(__dirname, '..', '..', 'build')));
 
-// Ruta para manejar solicitudes de cliente de React (SPA)
+// Ruta para manejar solicitudes del cliente de React (SPA)
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
-
-// // server.js
-// const express = require('express');
-// const session = require('express-session');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const authController = require('../controllers/authController');
-// const dataController = require('../controllers/dataController');
-
-// const app = express();
-
-// app.use(helmet());
-
-// const allowedOrigins = ['http://139.59.180.29:3000', 'http://localhost:3000'];
-
-// app.use(cors({
-//     credentials: true,
-//     origin: (origin, callback) => {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Origen no permitido por CORS'));
-//         }
-//     }
-// }));
-
-  
-
-// app.use(express.json());
-
-// // Configuración de express-session
-// app.use(session({
-//     secret: 'Bonica10_01', // Reemplaza 'tuSecreto' con una cadena secreta fuerte
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         secure: process.env.NODE_ENV === 'production',
-//         httpOnly: true
-//     }
-// }));
-
-// // Middleware isAuthenticated
-// const isAuthenticated = (req, res, next) => {
-//     if (req.session.userId) {
-//         next();
-//     } else {
-//         console.log('Usuario no autorizado'); // Agrega un registro de depuración
-//         res.status(401).json({ error: 'No autorizado' });
-//     }
-// };
-
-
-// // Rutas
-// app.post('/login', authController.login);
-// app.get('/new-luminaries', isAuthenticated, dataController.getNewLuminaries);
-// app.put('/update-luminaries/:id', isAuthenticated, dataController.updateLuminary);
-
-// // ... otras rutas y configuraciones ...
-
-// const PORT = process.env.PORT || 80;
-// app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
-
