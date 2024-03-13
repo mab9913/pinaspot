20139173121b09833a0cc4d7116035ec0a56d8f7
mab9913 <66171386+mab9913@users.noreply.github.com>
2024-03-13 17:35:26 +0100

    Update MapView.js

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -3,14 +3,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// No es necesario definir markerIcon si se usará únicamente createCustomMarkerIcon para todos los marcadores
-
 // Función para crear un marcador personalizado con una etiqueta permanente que muestra el ID
 const createCustomMarkerIcon = (labelText) => {
   return L.divIcon({
     html: `<div style="background-color: white; padding: 5px; border-radius: 5px; box-shadow: 1px 1px 5px rgba(0,0,0,0.5);">${labelText}</div>`,
     className: 'custom-div-icon',
-    iconSize: [30, 42], // Ajusta el tamaño según sea necesario
+    iconSize: L.point(30, 42), // Ajusta el tamaño según sea necesario
     iconAnchor: [15, 42], // Asegura que el icono se ancle correctamente
     popupAnchor: [0, -42], // Ajusta la posición del popup si es necesario
   });
@@ -45,23 +43,25 @@ const MapView = ({ isMapOpen, onLocationSelect, luminariesData }) => {
         attribution='&copy; OpenStreetMap contributors'
       />
       {luminariesData.map((luminary, index) => (
-        luminary.latitud !== null && luminary.longitud !== null && (
-<Marker position={[39.2666978950, -2.602043151]}>
-  <Popup>A simple Popup</Popup>
-</Marker>
-
+        luminary.latitud !== null && luminary.longitud !== null ? (
+          <Marker 
+            key={index} 
+            position={[luminary.latitud, luminary.longitud]} 
+            icon={createCustomMarkerIcon(luminary.id.toString())} // Usa la función para el ID como etiqueta permanente
+          >
             <Popup>
               {/* Personaliza el contenido del pop-up para mostrar la latitud y longitud */}
               Latitud: {luminary.latitud}<br />
               Longitud: {luminary.longitud}
             </Popup>
           </Marker>
-        )
+        ) : null
       ))}
     </MapContainer>
   );
 };
 
 export default MapView;
+
 
 
