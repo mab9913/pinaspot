f4aef68592424c1af90c47d510855db37b47896c
mab9913 <66171386+mab9913@users.noreply.github.com>
2024-03-18 11:13:15 +0100

    Update MapView.js

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -3,16 +3,13 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// Función para crear un icono de marcador personalizado con un símbolo y una etiqueta
-const createCustomMarkerIcon = (labelText) => {
+// Función para crear un icono de marcador personalizado con un símbolo de globo de marcador
+const createCustomMarkerIcon = () => {
   return L.divIcon({
     html: `
-      <div style="position: relative; text-align: center;">
-        <div style="color: black; position: absolute; width: 100%; top: -25px; left: 0;">${labelText}</div>
-        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="fill: #3388ff; width: 24px; height: 24px;">
-          <circle cx="50" cy="50" r="40"/>
-        </svg>
-      </div>
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="fill: #3388ff; width: 30px; height: 42px;">
+        <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/>
+      </svg>
     `,
     className: 'custom-div-icon',
     iconSize: L.point(30, 42),
@@ -54,7 +51,7 @@ const MapView = ({ isMapOpen, onLocationSelect, luminariesData }) => {
           <Marker 
             key={index} 
             position={[luminary.latitud, luminary.longitud]} 
-            icon={createCustomMarkerIcon(luminary.id.toString())} // Usa la función modificada para el marcador
+            icon={createCustomMarkerIcon()} // Usando el icono personalizado sin etiqueta
           >
             <Popup>
               Latitud: {luminary.latitud}<br />
@@ -68,6 +65,3 @@ const MapView = ({ isMapOpen, onLocationSelect, luminariesData }) => {
 };
 
 export default MapView;
-
-
-
